42bbaf76188be6d1db303bd23b24595e509d4f7b
Deividy Metheler <deividyz@gmail.com>
2015-10-01 20:09:04 -0300

    add test for .preco()

--- frete.spec.js
+++ frete.spec.js
@@ -39,7 +39,6 @@ describe("Frete", function () {
         f.prazo('13466321', function (err, results) {
             if (err) return done(err);
 
-
             let services = f.options.nCdServico;
             let hasAllServices = true;
 
@@ -82,6 +81,46 @@ describe("Frete", function () {
                 assert.equal(/Required option: sCepOrigem has invalid value/.test(err.message), true)
                 done()
             });
+        });
+    });
+
+    it('Request .preco()', function (done) {
+        let f = frete();
+
+        f
+            .peso(1)
+            .formato(1)
+            .comprimento(16)
+            .altura(2)
+            .largura(11)
+            .diametro(1)
+            .maoPropria('N')
+            .valorDeclarado(50)
+            .avisoRecebimento('S');
+
+        f.preco('13466321', function (err, results) {
+            if (err) return done(err);
+
+            let services = f.options.nCdServico;
+            let hasAllServices = true;
+
+            services.forEach(function (service) {
+                let hasService = false;
+                for (let i = 0; i < results.length; ++i) {
+                    if (results[i].Codigo == service) {
+                        hasService = true;
+                        break;
+                    }
+                }
+
+                if (!hasService) {
+                    hasAllServices = false;
+                }
+            });
+
+            assert.equal(hasAllServices, true);
+
+            done();
         });
     });
 });
