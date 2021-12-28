d052597e8220f90b845f03c0d02e213fa5c29f9a
Deividy Metheler Zachetti <deividyz@gmail.com>
2021-12-28 09:50:10 -0300

    add tests for new methods and some coode standards in spec file

--- frete.spec.js
+++ frete.spec.js
@@ -5,10 +5,10 @@ const frete = require('./frete');
 
 describe("Frete", function () {
     it('Test validation errors', function (done) {
-        var f = frete()
+        const f = frete()
 
         f.preco(function (err) {
-            let msg = err.message;
+            const msg = err.message;
 
             assert(/Validation error:/.test(msg));
 
@@ -28,8 +28,8 @@ describe("Frete", function () {
     });
 
     it('Set default options', function () {
-        let defaultOptions = frete.defaultOptions;
-        let expected = {
+        const defaultOptions = frete.defaultOptions;
+        const expected = {
             sCepOrigem: '',
             sCdMaoPropria: 'N',
             sCdAvisoRecebimento: 'N',
@@ -55,13 +55,13 @@ describe("Frete", function () {
     });
 
     it('Request .prazo() ok', function (done) {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         f.prazo('13466321', function (err, results) {
             if (err) return done(err);
 
-            let services = f.options.nCdServico;
-            let hasAllServices = true;
+            const services = f.options.nCdServico;
+            const hasAllServices = true;
 
             services.forEach(function (service) {
                 let hasService = false;
@@ -83,7 +83,7 @@ describe("Frete", function () {
     });
 
     it('Requests .prazo() validation errors', function (done) {
-        let f = frete().servico('');
+        const f = frete().servico('');
 
         f.prazo('13466321', function (err) {
             assert.equal(/Validation error/.test(err.message), true)
@@ -101,7 +101,7 @@ describe("Frete", function () {
     });
 
     it('Requests .prazo() correios error', function (done) {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         f.prazo('555555', function (err) {
             assert(/CEP de destino inexistente, consulte o Busca CEP./.test(err.message));
@@ -110,7 +110,7 @@ describe("Frete", function () {
     });
 
     it('Request .preco()', function (done) {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         f
             .peso(1)
@@ -126,7 +126,7 @@ describe("Frete", function () {
         f.preco('13466321', function (err, results) {
             if (err) return done(err);
 
-            let services = f.options.nCdServico;
+            const services = f.options.nCdServico;
             let hasAllServices = true;
 
             services.forEach(function (service) {
@@ -150,7 +150,7 @@ describe("Frete", function () {
     });
 
     it('Request .precoPrazo()', function (done) {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         f
             .peso(1)
@@ -166,7 +166,7 @@ describe("Frete", function () {
         f.precoPrazo('13466321', function (err, results) {
             if (err) return done(err);
 
-            let services = f.options.nCdServico;
+            const services = f.options.nCdServico;
             let hasAllServices = true;
 
             services.forEach(function (service) {
@@ -192,7 +192,7 @@ describe("Frete", function () {
     it('Request .preco() config object', function (done) {
         frete.cepOrigem('13467460').servico([ frete.codigos.pac ]);
 
-        var f = frete({
+        const f = frete({
             cepDestino: '13466321',
             peso: 1,
             formato: 1,
@@ -208,7 +208,7 @@ describe("Frete", function () {
         f.preco(function(err, results) {
             if (err) { return done(err); }
 
-            let services = f.options.nCdServico;
+            const services = f.options.nCdServico;
             let hasAllServices = true;
 
             services.forEach(function (service) {
@@ -234,7 +234,7 @@ describe("Frete", function () {
     it('[promises] support .preco()', async() => {
         frete.cepOrigem('13467460').servico([ frete.codigos.pac ]);
 
-        var f = frete({
+        const f = frete({
             cepDestino: '13466321',
             peso: 1,
             formato: 1,
@@ -249,7 +249,7 @@ describe("Frete", function () {
 
         const results = await f.preco({});
 
-        let services = f.options.nCdServico;
+        const services = f.options.nCdServico;
         let hasAllServices = true;
 
         services.forEach(function (service) {
@@ -270,11 +270,11 @@ describe("Frete", function () {
     });
 
     it('[promises] Request .prazo() ok', async() => {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         const results = await f.prazo('13466321');
 
-        let services = f.options.nCdServico;
+        const services = f.options.nCdServico;
         let hasAllServices = true;
 
         services.forEach(function (service) {
@@ -318,7 +318,7 @@ describe("Frete", function () {
     });
 
     it('[promises] request .precoPrazo()', async() => {
-        let f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
+        const f = frete().servico([ frete.servicos.sedex ]).cepOrigem('13467460');
 
         f
             .peso(1)
@@ -332,7 +332,7 @@ describe("Frete", function () {
             .avisoRecebimento('S');
 
         const results = await f.precoPrazo('13466321');
-        let services = f.options.nCdServico;
+        const services = f.options.nCdServico;
         let hasAllServices = true;
 
         services.forEach(function (service) {
@@ -355,7 +355,7 @@ describe("Frete", function () {
     it('[promises] request .preco() config object', async() => {
         frete.cepOrigem('13467460').servico([ frete.codigos.pac ]);
 
-        var f = frete({
+        const f = frete({
             cepDestino: '13466321',
             peso: 1,
             formato: 1,
@@ -370,7 +370,7 @@ describe("Frete", function () {
 
         const results = await f.preco();
 
-        let services = f.options.nCdServico;
+        const services = f.options.nCdServico;
         let hasAllServices = true;
 
         services.forEach(function (service) {
@@ -388,5 +388,32 @@ describe("Frete", function () {
         });
 
         assert.equal(hasAllServices, true);
+    });
+
+    it('listaServicos', async () => {
+        const results = await frete().listaServicos();
+        assert(results);
+
+        for (const service in frete.servicos) {
+            if (service === 'names') { continue; }
+            assert(results.some((r) => r.codigo == frete.servicos[service]));
+        }
+    });
+
+    it('listaServicosStar', async () => {
+        const results = await frete().listaServicosStar();
+        assert(results);
+
+    });
+
+    it('verificaModal', async () => {
+        const results = await frete({
+            cepOrigem: '13467460',
+            cepDestino: '13466321',
+        }).servico([ frete.servicos.sedex ]).verificaModal();
+
+        assert(results);
+        assert(results.length, 1);
+        assert(results[0].codigo, '04014');
     });
 });
